89a14a6112671b53ddde575d4923028b1b8307df
Alexchieme <alexchiemezie6@gmail.com>
2024-08-25 01:35:14 +0100

    first commit

---
+++ ja.js
@@ -0,0 +1,28 @@
+// /let name="alex";
+// var age = 49
+// console.log(name)
+// console.log(age)
+// var print=alert(age)
+// let code ="sonic"
+//     code="dog"
+// console.log(code)
+// const age = 20;
+
+// name = "hello"
+// console.log(name);
+// 
+// const profile={
+//     name:"solo",
+//     age: 46,
+//     reg_no:74
+// }
+// console.log(profile);
+// const profile =[1,2,3,4,5,"name"]
+
+// console.log(profile);
+// console.log(profile.push(6))
+// console.log(profile.unshift(7))
+// console.log(profile.pop())
+// console.log(profile.shift())
+// console.log(profile)
+
